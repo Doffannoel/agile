f09a0521764f6faa62ae13e89c0a658d18dc3a26
Natania Faylinn Anggunjaya <23502310020@student.prasetiyamulya.ac.id>
2025-02-19 17:01:15 +0700

    detailProject

--- blog_dosen/myapp/urls.py
+++ blog_dosen/myapp/urls.py
@@ -4,3 +4,7 @@ from . import views
 urlpatterns = [
     path('publikasi/', views.publikasi_view, name='publikasi'),
 ]
+
+urlpatterns = [
+    path('research/', views.detail_project, name='detail_project'),
+]
--- blog_dosen/myapp/views.py
+++ blog_dosen/myapp/views.py
@@ -5,4 +5,5 @@ def publikasi_view(request):
     publikasi_list = Publikasi.objects.all()
     return render(request, 'myapp\publikasi.html', {'publikasi_list': publikasi_list})
 
-
+def detail_project(request):
+    return render(request, 'myapp\detailProject.html')
