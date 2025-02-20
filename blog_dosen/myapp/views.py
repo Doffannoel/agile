393d62c20d2c478bc91fdc55d09dc04e2488e1b4
vigo <georgevigo2005@gmail.com>
2025-02-20 09:05:10 +0700

    benerin views

--- blog_dosen/myapp/views.py
+++ blog_dosen/myapp/views.py
@@ -3,13 +3,13 @@ from .models import Publikasi
 
 def publikasi_view(request):
     publikasi_list = Publikasi.objects.all()
-    return render(request, 'myapp\publikasi.html', {'publikasi_list': publikasi_list})
+    return render(request, 'myapp/publikasi.html', {'publikasi_list': publikasi_list})
 
 def detail_project(request):
-    return render(request, 'myapp\detailProject.html')
+    return render(request, 'myapp/detailProject.html')
 
 def landing_page(request):
-    return render(request, 'myapp\landingpage.html')
+    return render(request, 'myapp/landingpage.html')
 
 def pkm_(request):
-    return render(request, 'myapp\pkm.html')
+    return render(request, 'myapp/pkm.html')
